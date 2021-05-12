a077c3f562e2006e4988667a30abd37119fd7dff
Tianle Chen <tianlechen@gmail.com>
2021-05-11 22:38:24 -0400

    no openweathermap

--- stpd/__init__.py
+++ stpd/__init__.py
@@ -1,9 +1,7 @@
 from .climateweathergc import ClimateWeatherGC
-from .openweathermap import OpenWeatherMap
 from .simplemaps import SimpleMaps
 
 __all__ = [
     'ClimateWeatherGC',
-    'OpenWeatherMap',
     'SimpleMaps',
 ]
