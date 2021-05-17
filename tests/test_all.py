e0c1846f6d78666b2ebe39b4c6f476b9b4e5b678
Tianle Chen <tianlechen@gmail.com>
2021-05-16 22:59:13 -0400

    more osm features

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,6 +1,10 @@
 from datetime import date
 
+import numpy as np
+import pytest
+
 from stpd import NOAA, ClimateWeatherGC, OpenStreetMap, SimpleMaps
+from stpd.openstreetmap._osm_features import FEATURES as OSM_FEATURES
 
 # Toronto
 LAT = 43.6534817
@@ -24,6 +28,13 @@ def test_simplemaps():
     sm.get_features(LAT, LON)
 
 
-def test_openstreemap():
-    osm = OpenStreetMap(feature_names=['tree'])
+@pytest.mark.parametrize(
+    'feature_name',
+    [
+        pytest.param(n, id=n)
+        for n in np.random.choice(list(OSM_FEATURES.keys()), size=5, replace=False)
+    ]
+)
+def test_openstreemap(feature_name):
+    osm = OpenStreetMap(feature_names=[feature_name])
     osm.get_features(LAT, LON)
