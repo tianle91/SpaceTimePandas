644198a4281eaf200af28ef238b9113defc5cdb0
Tianle Chen <tianlechen@gmail.com>
2021-06-02 00:28:30 -0400

    update tests

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -4,24 +4,22 @@ import pandas as pd
 
 from stpd.location import OpenStreetMap, SimpleMaps
 from stpd.pipeline import Pipeline
-from stpd.utils_geocode import get_lat_lon
 from stpd.weather import NOAA, ClimateWeatherGC
 
-lat, lon = get_lat_lon('Toronto, Canada')
 df = pd.DataFrame({
-    'lat': lat,
-    'lon': lon,
+    'lat': 43.653482,
+    'lon': -79.383935,
     'date': [date(2020, 1, 1 + i) for i in range(2)]
 })
 
 
 p = Pipeline([
-    OpenStreetMap(),
-    SimpleMaps(),
-    NOAA(),
-    ClimateWeatherGC(),
+    OpenStreetMap(lat_col='lat', lon_col='lon'),
+    SimpleMaps(lat_col='lat', lon_col='lon'),
+    NOAA(date_col='date', lat_col='lat', lon_col='lon'),
+    ClimateWeatherGC(date_col='date', lat_col='lat', lon_col='lon'),
 ])
 
 
 def test_pipeline():
-    p.add_features_to_df(df, date_col='date', lat_col='lat', lon_col='lon')
+    p.add_features_to_df(df)
